4b382be97aef5c7e32d2f1e02239f7471063390f
al-jabir <aljabir098@gmail.com>
2025-02-12 13:04:21 +0600

    Enhance responsiveness and layout adjustments across various components and pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,12 +20,18 @@
   });
 
   var swiper = new Swiper('.mySwiper', {
-    slidesPerView: 2,
+    slidesPerView: 1,
     spaceBetween: 30,
     freeMode: true,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
     },
   });
 })(jQuery);
